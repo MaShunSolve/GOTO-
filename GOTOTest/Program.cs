dc4215c8059c85ae4fec5fbdfbf935de226575b1
MaShunSolve <108853084+MaShunSolve@users.noreply.github.com>
2022-10-09 11:59:41 +0800

    Create
    GOTO標籤小小測試

---
+++ GOTOTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (i > 9)
+                {
+                    goto BiggerThanNine;
+                }
+                Console.WriteLine(i);
+            }
+        BiggerThanNine:
+            Console.WriteLine("大於9");
+            Console.WriteLine("test123");
+        }
+    }
+}
